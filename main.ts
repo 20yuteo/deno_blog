2f37275308061def623264967e097a0a9f06ff00
20yuteo <20yuteo@gmail.com>
2022-08-15 16:17:23 +0900

    記事追加

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog@0.4.2/blog.tsx";
 
 blog({
-  title: "My Blog",
-  description: "This is my new blog.",
-  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  title: "20yuteo.log",
+  description: "東京でエンジニアをしています。ご連絡はtwitterで。",
+  // avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  avatar: "./favicons/eto_ushi_head.png",
   avatarClass: "rounded-full",
   author: "An author",
-
+  links: [
+    { title: "GitHub", url: "https://github.com/20yuteo" },
+    { title: "Twitter", url: "https://twitter.com/20yuteo" }
+  ],
+  lang: "ja",
   // middlewares: [
 
     // If you want to set up Google Analytics, paste your GA key here.
